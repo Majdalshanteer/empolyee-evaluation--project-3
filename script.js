4561e582c26eb3e399ecae259c7ce9e4d9ee79a0
Majdalshanteer <108790509+Majdalshanteer@users.noreply.github.com>
2022-08-29 23:03:26 +0300

    edit

--- script.js
+++ script.js
@@ -336,49 +336,42 @@ let re = 0;
 
 /* end of report */
 /* start of register script */
-function registerSubmit() {
-    // const form = document.getElementById("sign-up");
-    const emailField = document.getElementById("email");
-    const phoneField = document.getElementById("phone");
-    const passField = document.getElementById("pass");
-    const nameField = document.getElementById("name");
 
+const form = document.getElementById("sign-up");
+const emailField = document.getElementById("email");
+const phoneField = document.getElementById("phone");
+const passField = document.getElementById("pass");
+const nameField = document.getElementById("name");
 
+form.addEventListener("submit", (e) => {
 
-    if (nameField.value == null) {
+    if (nameField.value == null || emailField.value == "") {
         document.getElementById("name-error").textContent =
             "The name is required.";
-            console.log('name field is null')
-        return false;
+        e.preventDefault();
     }
     if (emailField.value == null || emailField.value == "") {
         document.getElementById("email-error").textContent =
             "The email field is required.";
-            console.log('email is null')
     } else if (
         !emailField.value.match(
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         )
-        
     ) {
         document.getElementById("email-error").textContent =
             "The email must be a valid email address.";
-            console.log('name field is not gmail')
-        return false;
+        e.preventDefault();
     } else {
         document.getElementById("email-error").textContent = "";
-        
     }
-    if (phoneField.value == "" || phoneField.value == null) {
+    if (phoneField.value == "" || phoneField.value == null ) {
         document.getElementById("tel-error").textContent =
             "The Mobile field is required.";
-            console.log('phone field is empty')
-        return false;
+        e.preventDefault();
     } else if (phoneField.value.length < 10) {
         document.getElementById("tel-error").textContent =
             "The mobile must be 10 characters.";
-            console.log('phone field is < 10')
-        return false;
+        e.preventDefault();
     } else {
         document.getElementById("tel-error").textContent = "";
     }
@@ -386,22 +379,19 @@ function registerSubmit() {
     if (passField.value == "" || passField.value == null) {
         document.getElementById("pass-error").textContent =
             "The password field is required.";
-            console.log('The password field is required.')
-        return false;
+        e.preventDefault();
     } else if (passField.value.length < 6) {
         document.getElementById("pass-error").textContent =
             "The password must be at least 6 characters.";
-            console.log('The password must be at least 6 characters.')
-        return false;
+        e.preventDefault();
     } else if (passField.value.length > 18) {
         document.getElementById("pass-error").textContent =
             "The password may not be greater than 18 characters.";
-            console.log('The password may not be greater than 18 characters.')
-        return false;
+        e.preventDefault();
     } else {
         document.getElementById("pass-error").textContent = "";
     }
-    console.log('formdata start')
+
     let formData = JSON.parse(localStorage.getItem("formData")) || [];
     formData.push({
         name: nameField.value,
@@ -410,10 +400,8 @@ function registerSubmit() {
         phone: phoneField.value,
     });
     localStorage.setItem("formData", JSON.stringify(formData));
-    console.log('formdata end')
- 
-
-}
+    
+});
 /* end of register page*/
 
 /* start of login page */
